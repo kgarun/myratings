eb20480e8af0603a6a7e9b68d3566bc1609e320d
kgarun <kgarun50@gmail.com>
2017-12-12 18:42:06 +0530

    Minor fix

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='myratings',
-    version='1.1.0',
+    version='1.1.1',
     description="Displays ratings of the user from various competitive programming sites in the terminal !!",
     long_description=readme + '\n\n' + history,
     author="Arun KG",
